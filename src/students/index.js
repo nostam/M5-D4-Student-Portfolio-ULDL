345a9c0c3a5f593891630a83b6a18f8b7fb4df8d
nostam <matsonho@gmail.com>
2020-12-08 15:24:45 +0100

    add get id & post method

--- src/students/index.js
+++ src/students/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { stringify } = require("querystring");
 const uniqid = require("uniqid");
+const { Z_BEST_SPEED } = require("zlib");
 
 const router = express.Router();
 
@@ -10,7 +12,28 @@ router.get("/", (req, res) => {
     .readFileSync(path.join(__dirname, "students.json"))
     .toString();
   const db = JSON.parse(rawFile);
-  res.status(200).send(JSON.parse(db));
+  res.status(200).send(db);
 });
 
+router.get("/:id", (req, res) => {
+  const rawFile = fs
+    .readFileSync(path.join(__dirname, "students.json"))
+    .toString();
+  const db = JSON.parse(rawFile);
+  const student = db.filter(
+    (student) => student.id === req.params.id.toString()
+  );
+  student.length > 0 ? res.status(200).send(student) : res.status(404).send();
+});
+
+router.post("/", (req, res) => {
+  const newEntry = { ...req.body, id: uniqid() };
+  const rawFile = fs
+    .readFileSync(path.join(__dirname, "students.json"))
+    .toString();
+  const db = JSON.parse(rawFile);
+  db.push(newEntry);
+  fs.writeFileSync(path.join(__dirname, "students.json"), JSON.stringify(db));
+  res.status(201).send({ id: newEntry.id });
+});
 module.exports = router;
