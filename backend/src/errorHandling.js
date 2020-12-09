57683fae0847b07b06ae463f48fe11321283acca
nostam <matsonho@gmail.com>
2020-12-09 16:25:43 +0100

    add validation for projects

--- backend/src/errorHandling.js
+++ backend/src/errorHandling.js
@@ -1,20 +1,20 @@
 const notFoundHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 404) {
-    res.status(404).send("Not found!");
+    res.status(404).send("Not found");
   }
   next(err);
 };
 
 const unauthorizedHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 401) {
-    res.status(401).send("Unauthorized!");
+    res.status(401).send("Unauthorized");
   }
   next(err);
 };
 
 const forbiddenHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 403) {
-    res.status(403).send("Forbidden!");
+    res.status(403).send("Forbidden");
   }
   next(err);
 };
--- backend/src/services/projects/index.js
+++ backend/src/services/projects/index.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const path = require("path");
 const uniqid = require("uniqid");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
+const { userInfo } = require("os");
+const { EINPROGRESS } = require("constants");
 
 readDb = (file) => {
   return JSON.parse(fs.readFileSync(path.join(__dirname, file)).toString());
@@ -23,41 +25,108 @@ router.get("/:id", (req, res, next) => {
   entry ? res.send(entry) : res.status(404).send();
 });
 
-router.post("/", (req, res, next) => {
-  const db = readDb("projects.json");
-  const newEntry = { ...req.body, id: uniqid(), creationDate: new Date() };
-  const students = readDb("../students/students.json");
-  const student = students.find((student) => student.id === req.body.studentId);
-  if (
-    Object.keys(student).length > 0 &&
-    student.hasOwnProperty("numberOfProjects")
-  ) {
-    student.numberOfProjects++;
-  } else {
-    student.numberOfProjects = 1;
+router.post(
+  "/",
+  [
+    body("name")
+      .isString()
+      .isLength({ min: 2 })
+      .withMessage("repo name too short")
+      .exists()
+      .withMessage("give repo a name"),
+    body("repoURL").isURL().withMessage("invalid url").exists(),
+    body("studentId").isString().isAlphanumeric(),
+  ],
+  (req, res, next) => {
+    try {
+      const err = validationResult(req);
+      if (!err.isEmpty()) {
+        const e = new Error();
+        e.message = { errors: err.array() };
+        e.http.StatusCode = 400;
+        console.log(e);
+        next(e);
+        // return res.status(400).send(e);
+      } else {
+        const db = readDb("projects.json");
+        const newEntry = {
+          ...req.body,
+          id: uniqid(),
+          creationDate: new Date(),
+        };
+        const students = readDb("../students/students.json");
+        const student = students.find(
+          (student) => student.id === req.body.studentId
+        );
+        if (
+          Object.keys(student).length > 0 &&
+          student.hasOwnProperty("numberOfProjects")
+        ) {
+          student.numberOfProjects++;
+        } else {
+          student.numberOfProjects = 1;
+        }
+        db.push(newEntry);
+        writeDb(db, "projects.json");
+        students
+          .filter((student) => student !== req.body.studentId)
+          .push(student);
+        writeDb(students, "../students/students.json");
+        res.status(201).send({ id: newEntry.id });
+      }
+    } catch (error) {
+      console.log("err by catch");
+      next(e);
+    }
   }
-  console.log(student);
-  db.push(newEntry);
-  writeDb(db, "projects.json");
-  students.filter((student) => student !== req.body.studentId).push(student);
-  writeDb(students, "../students/students.json");
-  res.status(201).send({ id: newEntry.id });
-});
+);
 
 router.delete("/:id", (req, res, next) => {
-  const db = readDb("projects.json");
-  const newDb = db.filter((entry) => entry.id !== req.params.id.toString());
-  writeDb(newDb);
-  res.status(204).send();
+  try {
+    const db = readDb("projects.json");
+    const newDb = db.filter((entry) => entry.id !== req.params.id.toString());
+    writeDb(newDb, "projects.json");
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:id", (req, res, next) => {
-  const db = readDb();
-  let entry = { ...req.body, id: req.params.id };
-  const newDb = db.filter((entry) => entry.id !== req.params.id.toString());
-  newDb.push(entry);
-  writeDb(newDb);
-  res.send(newDb);
-});
+router.put(
+  "/:id",
+  [
+    body("name")
+      .isString()
+      .isLength({ min: 2 })
+      .withMessage("repo name too short")
+      .exists()
+      .withMessage("give repo a name"),
+    body("repoURL").isURL().withMessage("invalid url").exists(),
+    body("studentId").isString().isAlphanumeric(),
+  ],
+  (req, res, next) => {
+    try {
+      const err = validationResult(req);
+      if (!err.isEmpty()) {
+        const e = new Error();
+        e.message = { errors: err.array() };
+        e.http.StatusCode = 400;
+        console.log(e);
+        next(e);
+      } else {
+        const db = readDb("projects.json");
+        let entry = { ...req.body, id: req.params.id };
+        const newDb = db.filter(
+          (entry) => entry.id !== req.params.id.toString()
+        );
+        newDb.push(entry);
+        writeDb(newDb, "projects.json");
+        res.send(newDb);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
